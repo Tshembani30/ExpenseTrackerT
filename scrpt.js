a3958fdba8529b11cd94584e2677c30a514e1370
Tshembani30 <thembaniasasent982@gmail.com>
2023-02-10 10:13:45 +0200

    expense-container remove saving and balance

--- scrpt.js
+++ scrpt.js
@@ -1,20 +1,9 @@
-let balanceIn = {
-    totalIncome: 0,
-    totalExpense: 0,
-    totalBalance: 0,
-    transactions: [],
-  }
+function expense()
+{
+  console.log("addAmt")
+}
 
-  getComputed()
-  
-  displayInHTML()
-
-  // must be showComputer with ID reference from the HTML and display
-  let displayIncome = document.getElementById('Balance Salary')
-  displayIncome.innerHTML = state.totalIncome
-
-  let displayExpense = document.getElementById('Expense')
-  displayExpense.innerHTML = state.totalExpense
-
-  let displayBalance = document.getElementById('Saving Profit')
-  displayBalance.innerHTML = state.totalBalance
+function income()
+{
+  console.log("addAmt")
+}
