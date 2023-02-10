2466cc183ba24088880dcc114c6d9b583670cc2e
Tshembani30 <thembaniasasent982@gmail.com>
2023-02-10 15:04:16 +0200

    added function getElementById

--- scrpt.js
+++ scrpt.js
@@ -1,8 +1,38 @@
 
+// add transaction display
+// function addTransaction(type) {
+    
+//   let detail = document.getElementById('detail')
+//   let amount = document.getElementById('amount')
+
+// }
+
+const balanceValue = document.getElementById("total-value");
+const totalBalance = document.getElementById("totalBalance");
+const form = document.getElementById("");
+const text = document.getElementById("");
+const amount = document.getElementById("");
+const money_minus = document.getElementById("");
+
+// const amountTransations=[
+//     {id:1,text:'salary',amount:3600},
+//     {id:2,text:'rent',amount:-1300},
+//     {id:3,text:'transport',amount:-1000},
+//     {id:4,text:'food',amount:-1500},
+//     {id:4,text:'clothing',amount:-500},
+// ]
 
 
+// function to get from calculate keep onclick to element
+function calculate(){
+  balance = expense - balance
+  console.log(balance)
+}
 
-
+function calculate(){
+  balance = income + balance
+  console.log(balance)
+}
 
 
 
